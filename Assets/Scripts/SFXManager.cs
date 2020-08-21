0e476d5f80b8c06f27892397dc890bfbb7e1870b
Tekknobot <thrillatron@hotmail.com>
2020-08-21 18:44:36 -0400

    Match game prototype.

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum Clip { Music, Select, Swap, Clear4, Clear5, Clear6, Clear7, Scratch};
+
+public class SFXManager : MonoBehaviour {
+	public static SFXManager instance;
+
+	private AudioSource[] sfx;
+
+	// Use this for initialization
+	void Start () {
+		instance = GetComponent<SFXManager>();
+		sfx = GetComponents<AudioSource>();
+    }
+
+	public void PlaySFX(Clip audioClip) {
+		sfx[(int)audioClip].Play();
+	}
+
+	public void StopSFX(Clip audioClip) {
+		sfx[(int)audioClip].Stop();
+	}	
+}
